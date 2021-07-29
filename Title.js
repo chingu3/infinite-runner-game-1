2b2b8662ed4a22d50bcc5587f537ac37c1b692ce
chingu3 <80951593+chingu3@users.noreply.github.com>
2021-07-29 12:46:19 +0400

    Add files via upload

---
+++ Title.js
@@ -0,0 +1,10 @@
+// class Title {
+
+//     constructor() {
+//       this.title = createElement('h2');
+//     }
+//     display(){
+//         this.title.html("Car Racing Game");
+//         this.title.position(displayWidth/2 - 50, 0);
+//     }
+// }
